57c471dcad3953a114a1f118eac250bd48be5460
cleo-cyber <kadimacleo92@gmail.com>
2022-05-18 01:15:50 +0300

    added a delete and update on todo items

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 
 # from os import abort
+
+# from crypt import methods
 import sys
 from flask import Flask, render_template, request,redirect,url_for,jsonify,abort
 from flask_sqlalchemy import SQLAlchemy
@@ -15,6 +17,7 @@ class Todo(db.Model):
     __tablename__='todos'
     id=db.Column(db.Integer,primary_key=True)
     decription=db.Column(db.String(),nullable=False)
+    # completed=db.Column(db.Boolean,nullable=False)
     
     def __repr__(self):
         return f'<Todo {self.id} {self.description}> '
@@ -46,7 +49,39 @@ def create_todo():
         else:
             return jsonify(body)
 
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed=request.get_json('completed')
+        todo=Todo.query.get(todo_id)
+        todo.completed=completed
+        # db.session.add(todo.completed)
+        db.session.commit()
+
+    except:
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+        return redirect(url_for('index'))
+    
+    
+@app.route('/todos/<delete_id>',methods=['DELETE'])
+def delete_todo(delete_id):
+    try:
+        # delete=Todo.id
+        Todo.query.filter_by(id=delete_id).delete()
+    #    Todo.query.filter_by(id=delete_id).delete()
+        db.session.commit()
+    except:
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+        return jsonify({'success':True})
+    
 @app.route('/')
 def index():
-    return render_template('index.html',data=Todo.query.all())
+    return render_template('index.html',data=Todo.query.order_by('id').all())
 
