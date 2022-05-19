bf31a01f4b1a9047202af88491628b455d9d2788
cleo-cyber <kadimacleo92@gmail.com>
2022-05-19 07:03:29 +0300

    changed file

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask_migrate import Migrate
 
 app=Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:Cleo1999*@localhost:5432/todoapp'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:abc*@localhost:5432/todoapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 db=SQLAlchemy(app)
